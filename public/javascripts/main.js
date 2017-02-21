9c299c1856aa858be46b6d215440cf76eca5f5d2
suphler <taka.suphler@gmail.com>
2017-02-21 16:24:47 +0200

    5th commit
    Divide  to blocks (boot, Preload etc)
    little changes

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -114,9 +114,8 @@ game.state.add('menu', menuState);
 
 game.state.add('boot', bootState);
 game.state.add('load', loadState);
-game.state.add('menu', menuState);
-// game.state.add('play', playState);
-// Start the 'boot' state
+// game.state.add('menu', menuState);
+
 game.state.start('boot');
 // game.state.start('main');
 
